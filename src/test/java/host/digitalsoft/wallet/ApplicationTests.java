d3dd24946914d040283f024be743260bfceec461
Mahbub <mahbub.alam@trustaxiatapay.com>
2021-10-06 13:10:11 +0600

    added dummy test

--- src/test/java/host/digitalsoft/wallet/ApplicationTests.java
+++ src/test/java/host/digitalsoft/wallet/ApplicationTests.java
@@ -1,13 +1,33 @@
 package host.digitalsoft.wallet;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
-	/*@Test
-	void contextLoads() {
+    /*@Test
+    void calculateSum_basic() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        int actualResult = business.calculateSum(new int[]{1, 2, 3});
+        int expectedResult = 6;
+        assertEquals(expectedResult, actualResult);
+    }
 
-	}*/
+    @Test
+    void calculateSum_empty() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        int actualResult = business.calculateSum(new int[]{});
+        int expectedResult = 0;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void calculateSum_onevalue() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        int actualResult = business.calculateSum(new int[]{5});
+        int expectedResult = 5;
+        assertEquals(expectedResult, actualResult);
+    }*/
 
 }
